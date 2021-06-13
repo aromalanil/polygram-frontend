8801e9728ef03b5f1adb22cb0bdf354fc770a7e6
Aromal Anil <aromalanilkannan@gmail.com>
2021-06-13 17:22:36 +0530

    Change logo

--- src/components/common/Logo/index.jsx
+++ src/components/common/Logo/index.jsx
@@ -1,6 +1,17 @@
+import { useRhinoValue } from '../../../global/state';
+
 const Logo = ({ width, className }) => {
+  const theme = useRhinoValue('theme');
+
   const logoStyle = { width };
-  return <img style={logoStyle} src="/assets/logo.svg" className={className} alt="Polygram Logo" />;
+  return (
+    <img
+      style={logoStyle}
+      alt="Polygram Logo"
+      className={className}
+      src={theme?.isDarkMode ? '/assets/logo_dark.svg' : '/assets/logo_light.svg'}
+    />
+  );
 };
 
 export default Logo;
