c4e8f8d550802f3263041eac4d5bef8fdce20150
Aromal Anil <aromalanilkannan@gmail.com>
2021-07-14 15:37:24 +0530

    Remove autofocus from search

--- src/components/pages/Search/index.jsx
+++ src/components/pages/Search/index.jsx
@@ -25,12 +25,7 @@ const Search = () => {
 
   return (
     <>
-      <SearchBar
-        tab={activeTab}
-        className="large-search-bar"
-        autoFocus
-        initialQuery={searchQuery}
-      />
+      <SearchBar tab={activeTab} className="large-search-bar" initialQuery={searchQuery} />
       <Tab
         activeTab={activeTab}
         tabNames={['Questions', 'Topics']}
