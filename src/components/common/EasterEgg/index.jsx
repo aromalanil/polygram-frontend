18b0544640f510ff6d185edc8d4e536a9a9817fb
Aromal Anil <aromalanilkannan@gmail.com>
2026-06-18 23:53:40 +0530

    feat: fix the easter egg

--- src/components/common/EasterEgg/index.jsx
+++ src/components/common/EasterEgg/index.jsx
@@ -1,26 +1,37 @@
 /* eslint-disable  no-console */
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
+import { DEVELOPERS_IMAGE } from './developersImage';
 
 function consoleWithoutSource(...args) {
   setTimeout(console.log.bind(console, ...args));
 }
 
+let hasLogged = false;
+
 const EasterEgg = () => {
-  if (import.meta.env.PROD) {
+  useEffect(() => {
+    if (hasLogged) return;
+    hasLogged = true;
+
     consoleWithoutSource(
-      '%cThey are here',
+      '%cThey are here!',
       `background-color: #151A26; 
-     padding: 5px;
-     font-size: 30px;
-     border-left: 5px solid #7D71FE`
+       color: #7D71FE;
+       padding: 8px 12px;
+       font-size: 20px;
+       font-weight: bold;
+       border-left: 5px solid #7D71FE;
+       border-radius: 4px;`
     );
     consoleWithoutSource(
-      '%c+',
-      `padding: 60px 110px; 
-     line-height: 50px; 
-     background: url(https://i.imgur.com/gbHzjkD.gif);`
+      '%c ',
+      `padding: 30px 40px; 
+       line-height: 60px; 
+       background-image: url(${DEVELOPERS_IMAGE});
+       background-size: contain;
+       background-repeat: no-repeat;`
     );
-  }
+  }, []);
 
   return null;
 };
